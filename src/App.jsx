1f41f0c8c2ac5e778051ae68d82481715f80cab6
SamueleTerraroli <terraroli99@gmail.com>
2025-02-07 13:57:22 +0100

    feat: adddefault layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefalultLayout from "./layouts/DefalultLayout"
+import HomePage from "./pages/HomePage"
+import Error404 from "./pages/Error404"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefalultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path="*" Component={Error404} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
